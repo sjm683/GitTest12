dc7212946bd00a28a1af9ab14f30aae88dc044b3
sjm9451 <sjm3109@gmail.com>
2018-11-28 18:12:29 +0900

    버튼2

--- GitTest/Form1.cs
+++ GitTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GitTest
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("버튼2 출력");
+        }
     }
 }
